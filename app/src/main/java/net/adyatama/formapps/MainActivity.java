9a59ef6be4e81a1f7336b52b53480dd709f20994
Adyatama-IT <adyatama@students.amikom.ac.id>
2021-12-04 14:17:32 +0700

    first commit

---
+++ app/src/main/java/net/adyatama/formapps/MainActivity.java
@@ -0,0 +1,26 @@
+package net.adyatama.formapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText inputName = (EditText) findViewById(R.id.id_input_nama);
+        EditText inputEmail = (EditText) findViewById(R.id.id_input_email);
+        EditText inputPhone = (EditText) findViewById(R.id.id_input_phone);
+        EditText inputPassword = (EditText) findViewById(R.id.id_input_pwd);
+
+        Button btnSave = (Button) findViewById(R.id.in_button_save);
+        
+    }
+}
